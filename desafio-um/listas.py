ba528c397717c78d76658aa87f9c16c5a78cfd90
Idelândia Rangel <idelandia-santos@hotmail.com>
2023-09-18 15:53:31 -0300

    commit

--- desafio-um/listas.py
+++ desafio-um/listas.py
@@ -40,3 +40,29 @@ else:
     print("False")
 
 #4
+respostas_positivas = 0
+
+perguntas = [
+    "Telefonou para a vítima? ",
+    "Esteve no local do crime? ",
+    "Mora perto da vítima? ",
+    "Devia para a vítima? ",
+    "Já trabalhou com a vítima? "
+]
+
+for pergunta in perguntas:
+   resposta = input(pergunta = "(Sim/Não)").lower()
+if resposta == "sim":
+       respostas_positivas += 1
+
+if respostas_positivas == 5:
+    classificacao = "Assassino."
+elif respostas_positivas >= 3:
+    classificacao = "Cúmplice."
+elif respostas_positivas >=2:
+    classificacao = "Suspeito"
+else:
+    classificacao = "Inocente"
+
+print(f"Classificação: {classificacao}") 
+
